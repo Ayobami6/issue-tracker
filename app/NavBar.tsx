2ec453938f26354faf39e3ca6c6cd86d7408adc7
Ayobami <ayobamidele006@gmail.com>
2023-10-28 16:59:04 +0100

    add navbar blueprint

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 export const NavBar = () => {
-	return <nav></nav>;
+	return (
+		<nav>
+			<Link href={'/'}>Home</Link>
+			<ul>
+				<li>
+					<Link href={'/'}>Dashboard</Link>
+				</li>
+				<li></li>
+			</ul>
+		</nav>
+	);
 };
