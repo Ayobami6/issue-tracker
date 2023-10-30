a7b5b57bf13e08cf7cf3c2e3dfd541da73a95bb2
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-10-30 11:22:48 +0000

    Update 4 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
-	return <div>Hello Next</div>;
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
