66c4129d3a81a6a89eb7710117db79d73e94ee3b
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-10-30 15:01:55 +0000

    add navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import { GoIssueClosed } from 'react-icons/go'
 
-export const NavBar = () => {
+export const NavBar: React.FC = () => {
+	const links = [
+		{ label: 'Dashboard', href: '/' },
+		{ label: 'Issues', href: '/issues' }
+	]
 	return (
-		<nav>
-			<Link href={'/'}>Home</Link>
-			<ul>
-				<li>
-					<Link href={'/'}>Dashboard</Link>
-				</li>
-				<li></li>
+		<nav className='flex space-x-6 border-b text-2xl mb-5 px-5 items-center h-14'>
+			<Link href={'/'}><GoIssueClosed className="text-blue-400 text-3xl" /></Link>
+			<ul className='flex space-x-6'>
+				{links.map((link, index) => (
+					<li key={index}>
+						<Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>
+					</li>
+
+				))}
 			</ul>
 		</nav>
 	);
