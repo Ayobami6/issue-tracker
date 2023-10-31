f74b678baf8053f0882a33221ef4099086a935f9
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-10-31 13:17:42 +0000

    add radix theme component wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
 
       <body className={`${inter.className}`}>
         <Theme>
@@ -27,7 +27,9 @@ export default function RootLayout({
             {children}
           </main>
         </Theme>
+
       </body>
+
 
     </html>
   )
