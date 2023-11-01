ce091393f2a68ea2a6da2948b22e7dc1506e2c3d
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-11-01 19:32:57 +0000

    extract error validation feedback to a component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createIssueSchema } from '@/app/createIssueSchema';
 import { z } from 'zod'
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 
 type IssueForm = z.infer<typeof createIssueSchema>
@@ -49,9 +50,13 @@ const NewIssuePage = () => {
                     </TextField.Input>
 
                 </TextField.Root>
-                {errors.title && <Text as='p' color='red'>{errors.title.message}</Text>}
+                <ErrorMessage>
+                    {errors.title?.message}
+                </ErrorMessage>
                 <Controller name='description' control={control} render={({ field }) => <SimpleMDE placeholder='Description' {...field} />} />
-                {errors.title && <Text color='red' as='p'>{errors.description?.message}</Text>}
+                <ErrorMessage>
+                    {errors.description?.message}
+                </ErrorMessage>
                 <Button>Submit an issue</Button>
             </form>
         </div>
